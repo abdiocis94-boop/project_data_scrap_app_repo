0bac85251f00f42c8e0cddbb42ea82fe8bb835e0
abdiocis94-boop <abdiocis94@gmail.com>
2026-01-06 03:09:30 +0000

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 #import plotly.express as px
-#import plotly.graph_objects as go
+#####import plotly.graph_objects as go
 import numpy as np
 import io
 import json
@@ -849,4 +849,5 @@ def main():
     
 
     # Page d'évaluation
+
 
